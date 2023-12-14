07f94b58cb41fcdba40a66bc015b59d13bcd1b78
dev-aram <dev.aram0717@gmail.com>
2023-12-14 15:53:51 +0900

    약국 및 119 수정

--- server/admin/data/report.js
+++ server/admin/data/report.js
@@ -8,23 +8,23 @@ export async function generateEmergencyMessage(id) {
 
     if (user) {
       if (user.name) {
-        emergencyMessage += `이름: ${user.name}\n`;
+        emergencyMessage += `이름: ${user.name} /`;
       }
       if (user.birthdate) {
-        emergencyMessage += `생년월일: ${user.birthdate}\n`;
+        emergencyMessage += `생년월일: ${user.birthdate} /`;
       }
       if (user.bloodType) {
-        emergencyMessage += `혈액형: ${user.bloodType}형\n`;
+        emergencyMessage += `혈액형: ${user.bloodType}형 /`;
       }
       // underlyingDisease가 값이 비어있는데 기저질환: 이 메시지에 포함되는 현상이 나타나서 length 조건 추가
       if (user.underlyingDisease && user.underlyingDisease.length > 0) {
-        emergencyMessage += `기저질환: ${user.underlyingDisease.join(', ')}\n`;
+        emergencyMessage += `기저질환: ${user.underlyingDisease.join(', ')} /`;
       }
       if (user.allergy && user.allergy.length > 0) {
-        emergencyMessage += `알러지: ${user.allergy.join(', ')}\n`;
+        emergencyMessage += `알러지: ${user.allergy.join(', ')} /`;
       }
       if (user.medication && user.medication.length > 0) {
-        emergencyMessage += `복용약: ${user.medication.join(', ')}\n`;
+        emergencyMessage += `복용약: ${user.medication.join(', ')} /`;
       }
     }
     emergencyMessage += '긴급상황입니다.'
@@ -37,4 +37,3 @@ export async function generateEmergencyMessage(id) {
     return null;
   }
 }
-
